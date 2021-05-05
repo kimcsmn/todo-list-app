1d564138e1542d2c01b79c4836525856c110a0a6
kimcsmn <kim.csmn@gmail.com>
2021-05-05 02:44:34 -0700

    added event listener for input

---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+const list = document.querySelector('.list');
+const input = document.querySelector('.input');
+
+input.addEventListener('keypress', function(e){
+  if (e.key === 'Enter'){
+
+    // create a li
+    const li = document.createElement('li');
+    const item = document.createElement('div');
+    const checkBtn = document.createElement('i');
+    const deleteBtn = document.createElement('i')
+    item.classList.add('item')
+    checkBtn.classList.add('fas', 'fa-check-square');
+    deleteBtn.classList.add('fas', 'fa-window-close');
+    li.innerHTML = e.target.value;
+    input.value = "";
+    item.append(checkBtn);
+    item.append(li);
+    item.append(deleteBtn);
+    list.append(item)
+
+
+    checkBtn.addEventListener('click', function(e){
+    e.target.parentElement.children[1].classList.toggle('check');
+  })
+  }
+
+
+ 
+})
